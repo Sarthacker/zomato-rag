6cea801e0f4c575d24d758d6e3a6f4b3c8cc07c2
Sarthak Kumar Singh <sks19june@gmail.com>
2025-04-27 21:30:13 +0530

    Deployment changes

--- src/config.py
+++ src/config.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 from pinecone import Pinecone
 from groq import Groq
+import streamlit as st
 
 load_dotenv()
 
-HUGGING_FACE_MODEL = os.getenv("HUGGING_FACE_MODEL")
-LLAMA_MODEL = os.getenv("LLAMA_MODEL")
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_INDEX = os.getenv("PINECONE_INDEX")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HUGGING_FACE_MODEL = st.secrets["HUGGING_FACE_MODEL"]
+LLAMA_MODEL = st.secrets["LLAMA_MODEL"]
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+PINECONE_INDEX = st.secrets["PINECONE_INDEX"]
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 
 pinecone_client = Pinecone(api_key=PINECONE_API_KEY)
